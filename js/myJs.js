1efb91bbe030af28e0412b1c0ea20cfb6c89e3fd
Duynnnn <98943236+Duynnnn@users.noreply.github.com>
2022-02-05 22:01:57 +0700

    Duyn

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "Chào bạn Hưng",
+  text2: "Bạn Duyn có điều muốn nói",
+  text3: "Anh có yêu em hong???",
+  text4: "Trả lời hoặc ăn đấm nha ☠️",
+  text5: "Yêu sương sương thôi ◣_◢",
+  text6: "Yêu ơi là yêuuuuuu ❤️",
+  text7: "Nhưng sao lại yêu cơ",
+  text8: "Ỏoooooo",
+  text9: "Vì em ngoan, hiền, dịu dàng lại còn đảm đang",
+  text10: "Hí hí em biết mà. Em cũng yêu anh",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "Happy our first Valentine!!!",
+  text12: "Ấn vào đây (Pass là số ngày mình iu nhau)",
 };
 
 $(document).ready(function () {
